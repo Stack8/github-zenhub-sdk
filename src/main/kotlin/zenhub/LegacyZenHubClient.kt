7ffbb68f42b7e7751787a1354af4c41763f1aa1c
Arturo Rotondo <3749826+arotondo@users.noreply.github.com>
2023-12-15 13:55:04 -0500

    Remove legacy code

--- src/main/kotlin/zenhub/LegacyZenHubClient.kt
+++
@@ -1,5 +0,0 @@
-package zenhub
-
-import shared.GraphQueryHttpClient
-
-internal object LegacyZenHubClient : GraphQueryHttpClient("https://api.zenhub.com/public/graphql", System.getenv("ZENHUB_GRAPHQL_TOKEN"))
