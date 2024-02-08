6dbc7686aeb7395115df093ad902dd682ff926a5
Arturo Rotondo <3749826+arotondo@users.noreply.github.com>
2024-02-08 16:36:59 -0500

    Use defaults in smoke test

--- src/main/kotlin/github/GitHubClientSmokeTest.kt
+++ src/main/kotlin/github/GitHubClientSmokeTest.kt
@@ -2,7 +2,7 @@ package github
 
 fun main() {
     GitHubClient().use { client ->
-        val repository = client.getRepository("smacs", "stack8")
+        val repository = client.getRepository()
         if (repository != null) {
             println("repository: $repository")
             println(repository.milestones?.nodes?.get(0)?.title)
