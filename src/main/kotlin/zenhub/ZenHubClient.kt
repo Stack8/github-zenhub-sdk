7fdc443e2e05834bdc7fda40890992a88a3aff7d
Arturo Rotondo <3749826+arotondo@users.noreply.github.com>
2024-02-08 15:57:34 -0500

    Extract repository constants from ZenHubClient.
    This will allow us to easily test against some alternative test repository.

--- src/main/kotlin/zenhub/ZenHubClient.kt
+++ src/main/kotlin/zenhub/ZenHubClient.kt
@@ -9,19 +9,25 @@ import kotlinx.coroutines.runBlocking
 import okhttp3.internal.closeQuietly
 import java.time.Instant
 
-class ZenHubClient : AutoCloseable {
+class ZenHubClient(
+    private val githubRepositoryId: Int,
+    private val gitRepositoryId: String,
+    private val zenhubWorkspaceId: String
+) : AutoCloseable {
 
-    private val apolloClient: ApolloClient = ApolloClient.Builder().serverUrl("https://api.zenhub.com/public/graphql")
-        .addHttpHeader("Authorization", "Bearer ${System.getenv("ZENHUB_GRAPHQL_TOKEN")}")
+    private val apolloClient: ApolloClient = ApolloClient
+        .Builder()
+        .serverUrl("https://api.zenhub.com/public/graphql")
+        .addHttpHeader(
+            "Authorization",
+            "Bearer ${System.getenv("ZENHUB_GRAPHQL_TOKEN")}"
+        )
         .build()
 
-    object Constants {
-        const val GITHUB_REPOSITORY_ID: Int = 15617306
-        const val GIT_REPOSITORY_ID: String = "Z2lkOi8vcmFwdG9yL1JlcG9zaXRvcnkvMjEwNTg"
-        const val ZENHUB_WORKSPACE_ID: String = "59c54eb49d9e774e473597f1"
-    }
-
-    fun searchClosedIssuesBetween(startTime: Instant, endTime: Instant): List<SearchClosedIssuesQuery.Node> {
+    fun searchClosedIssuesBetween(
+        startTime: Instant,
+        endTime: Instant
+    ): List<SearchClosedIssuesQuery.Node> {
         var results = emptyList<SearchClosedIssuesQuery.Node>()
         var earliestClosedDate: Instant
         var cursor: String? = null
@@ -39,7 +45,7 @@ class ZenHubClient : AutoCloseable {
     }
 
     fun issueByInfo(issueNumber: Int): IssueByInfoQuery.IssueByInfo? = runBlocking {
-        val query = IssueByInfoQuery(Constants.GITHUB_REPOSITORY_ID, Constants.GIT_REPOSITORY_ID, issueNumber)
+        val query = IssueByInfoQuery(githubRepositoryId, gitRepositoryId, issueNumber)
         apolloClient.query(query).toFlow().single().data?.issueByInfo
     }
 
@@ -47,22 +53,32 @@ class ZenHubClient : AutoCloseable {
         apolloClient.closeQuietly()
     }
 
-    private fun searchClosedIssues(after: String?): SearchClosedIssuesQuery.SearchClosedIssues? = runBlocking {
-        val query = SearchClosedIssuesQuery(Constants.ZENHUB_WORKSPACE_ID, 100, Optional.presentIfNotNull(after))
-        apolloClient.query(query).toFlow().single().data?.searchClosedIssues
-    }
+    private fun searchClosedIssues(after: String?): SearchClosedIssuesQuery.SearchClosedIssues? =
+        runBlocking {
+            val query =
+                SearchClosedIssuesQuery(zenhubWorkspaceId, 100, Optional.presentIfNotNull(after))
+            apolloClient.query(query).toFlow().single().data?.searchClosedIssues
+        }
 
-    private fun trimResults(results: List<SearchClosedIssuesQuery.Node>, startDate: Instant, endDate: Instant): List<SearchClosedIssuesQuery.Node> {
+    private fun trimResults(
+        results: List<SearchClosedIssuesQuery.Node>,
+        startDate: Instant,
+        endDate: Instant
+    ): List<SearchClosedIssuesQuery.Node> {
         // The results are returned in reverse chronological order.
         if (results.isEmpty()) {
             return results
         }
 
-        val indexOfEarliestIssue = results.indexOfFirst { issue -> Instant.parse(issue.closedAt.toString()).isBefore(startDate) }
+        val indexOfEarliestIssue = results.indexOfFirst { issue ->
+            Instant.parse(issue.closedAt.toString()).isBefore(startDate)
+        }
 
         var indexOfLatestIssue = -1
         if (Instant.parse(results[0].closedAt.toString()).isAfter(endDate)) {
-            indexOfLatestIssue = results.indexOfLast { issue -> Instant.parse(issue.closedAt.toString()).isAfter(endDate) }
+            indexOfLatestIssue = results.indexOfLast { issue ->
+                Instant.parse(issue.closedAt.toString()).isAfter(endDate)
+            }
         }
 
         return results.subList(indexOfLatestIssue + 1, indexOfEarliestIssue)
