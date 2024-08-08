2bad5a2524513ef50bedec4d13e23560e76f913e
Justin Sadakhom <justin.sadakhom@gmail.com>
2024-08-08 16:12:58 -0400

    Add more integrations tests

--- src/test/kotlin/zenhub/ZenHubClientTest.kt
+++ src/test/kotlin/zenhub/ZenHubClientTest.kt
@@ -7,6 +7,17 @@ class ZenHubClientTest {
     private val zenHubClient = ZenHubClient()
 
     @Test
+    fun whenGetIssuesByPipelineThenAtLeastZeroIssues() {
+        val result = zenHubClient.getIssuesByPipeline(Pipeline.MERGE_READY)
+
+        if (!result.isNullOrEmpty()) {
+            assertTrue(result[0].number > 0)
+        } else {
+            assertTrue(result.isNullOrEmpty())
+        }
+    }
+
+    @Test
     fun whenGetActiveReleasesThenAtLeastOneRelease() {
         zenHubClient.getActiveReleases()?.let { assertTrue(it.isNotEmpty()) }
     }
