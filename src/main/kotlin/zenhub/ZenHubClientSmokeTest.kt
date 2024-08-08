8930dbae26b40e471ec91ac6d34878b48300cdac
Justin Sadakhom <justin.sadakhom@gmail.com>
2024-08-08 16:14:01 -0400

    Restore original smoke test

--- src/main/kotlin/zenhub/ZenHubClientSmokeTest.kt
+++ src/main/kotlin/zenhub/ZenHubClientSmokeTest.kt
@@ -1,9 +1,27 @@
 package zenhub
 
+import java.time.Duration
+import java.time.Instant
+
 fun main() {
     ZenHubClient().use { client ->
-        println(client.getIssuesByPipeline(Pipeline.MERGE_READY))
-        println(client.getActiveReleases())
+        val issue = client.issueByInfo(15675)
+        if (issue != null) {
+            println("id: ${issue.id}")
+            println("body: ${issue.body}")
+        }
+        val fourteenDays = Duration.ofDays(28)
+        val response = client.searchClosedIssuesBetween(
+            Instant.now().minus(fourteenDays),
+            Instant.now()
+        )
+        println("Results Size: ${response.size}")
+
+        response.forEach { node -> println(node) }
+
+        val currentSprint = client.getCurrentSprint()
+        println(currentSprint)
+
+        println(client.getPreviousSprint())
     }
 }
-
