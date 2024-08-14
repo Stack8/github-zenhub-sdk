b387a3ce74c19096e962a27f7b2b370688b45095
Justin Sadakhom <justin.sadakhom@gmail.com>
2024-08-14 09:35:02 -0400

    Remove useless query

--- src/main/kotlin/github/GitHubClient.kt
+++ src/main/kotlin/github/GitHubClient.kt
@@ -2,7 +2,6 @@ package github
 
 import com.apollographql.apollo3.ApolloClient
 import com.ziro.engineering.github.graphql.sdk.GetBranchLogHistoryQuery
-import com.ziro.engineering.github.graphql.sdk.GetFileFromBranchQuery
 import com.ziro.engineering.github.graphql.sdk.RepositoryQuery
 import kotlinx.coroutines.flow.single
 import kotlinx.coroutines.runBlocking
@@ -25,16 +24,6 @@ class GitHubClient : AutoCloseable {
     ): RepositoryQuery.Repository? = runBlocking {
         val query = RepositoryQuery(repoName, repoOwner)
         apolloClient.query(query).toFlow().single().data?.repository
-    }
-
-    fun getFileFromBranch(
-        repoOwner: String = DEFAULT_GITHUB_REPOSITORY_OWNER,
-        repoName: String = DEFAULT_GITHUB_REPOSITORY_NAME,
-        branch: String,
-        filePath: String,
-    ): String? = runBlocking {
-        val query = GetFileFromBranchQuery(repoOwner, repoName, "$branch:$filePath")
-        apolloClient.query(query).toFlow().single().data?.repository?.`object`?.onBlob?.text
     }
 
     /**
