20f8c16b6dc78027e326a04f0551ae88468869d7
Justin Sadakhom <32400122+justin-sadakhom@users.noreply.github.com>
2024-11-07 14:52:12 -0500

    Issue #18124 - Rejected issues are often not moved from Merge Ready to UAT Ready (PR #7)
    * Add support for dynamic number of pages when getting commits
    
    * Address code review feedback and bump version

--- src/main/kotlin/github/GitHubClient.kt
+++ src/main/kotlin/github/GitHubClient.kt
@@ -1,12 +1,17 @@
 package github
 
 import com.apollographql.apollo3.ApolloClient
+import com.apollographql.apollo3.api.Optional
 import com.ziro.engineering.github.graphql.sdk.GetBranchLogHistoryQuery
 import com.ziro.engineering.github.graphql.sdk.GetFileFromBranchQuery
 import com.ziro.engineering.github.graphql.sdk.RepositoryQuery
 import kotlinx.coroutines.flow.single
 import kotlinx.coroutines.runBlocking
 import okhttp3.internal.closeQuietly
+import kotlin.math.ceil
+import kotlin.math.min
+
+const val MAX_COMMITS_IN_PAGE = 100
 
 private const val DEFAULT_GITHUB_REPOSITORY_NAME = "smacs"
 private const val DEFAULT_GITHUB_REPOSITORY_OWNER = "stack8"
@@ -37,18 +42,35 @@ class GitHubClient : AutoCloseable {
         apolloClient.query(query).toFlow().single().data?.repository?.`object`?.onBlob?.text
     }
 
-    /**
-     * Note: this only returns the latest 100 commits, not the full commit history.
-     */
-    fun getRecentCommits(
+    fun getCommits(
         repoOwner: String = DEFAULT_GITHUB_REPOSITORY_OWNER,
         repoName: String = DEFAULT_GITHUB_REPOSITORY_NAME,
         branch: String,
+        numCommits: Int
     ): List<String> = runBlocking {
-        val query = GetBranchLogHistoryQuery(repoOwner, repoName, branch)
-        apolloClient.query(query).toFlow().single().data?.repository?.ref?.target?.onCommit?.history?.edges?.mapNotNull {
-            it?.node?.message
-        } ?: emptyList()
+        val commits = mutableListOf<String>()
+        var hasNextPage = true
+        var cursor = Optional.absent<String>()
+        var commitsLeft = numCommits
+
+        while (commitsLeft > 0 && hasNextPage) {
+            val numCommitsInPage = min(MAX_COMMITS_IN_PAGE, commitsLeft)
+            val query = GetBranchLogHistoryQuery(repoOwner, repoName, branch, numCommitsInPage, cursor)
+            val response = apolloClient.query(query).toFlow().single()
+
+            commits.addAll(response.data?.repository?.ref?.target?.onCommit?.history?.edges?.mapNotNull {
+                it?.node?.message
+            } ?: emptyList())
+
+            response.data?.repository?.ref?.target?.onCommit?.history?.pageInfo?.let {
+                cursor = Optional.presentIfNotNull(it.endCursor)
+                hasNextPage = it.hasNextPage
+            }
+
+            commitsLeft -= numCommitsInPage
+        }
+
+        commits
     }
 
     override fun close() {
