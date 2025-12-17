e10ee91afd2cc4dbbe573b9fb7d94e3c9de14029
Justin Sadakhom <justin.sadakhom@gmail.com>
2025-12-17 11:04:11 -0500

    Add ability to update pull request base branch

--- src/main/kotlin/github/GitHubClient.kt
+++ src/main/kotlin/github/GitHubClient.kt
@@ -163,10 +163,11 @@ class GitHubClient : AutoCloseable {
             ?.pullRequestFragment
     }
 
-    fun updatePullRequest(id: String, body: String?, state: PullRequestUpdateState?) = runBlocking {
+    fun updatePullRequest(id: String, baseBranch: String?, body: String?, state: PullRequestUpdateState?) = runBlocking {
         val input =
             UpdatePullRequestInput(
                 pullRequestId = id,
+                baseRefName = Optional.presentIfNotNull(baseBranch),
                 body = Optional.presentIfNotNull(body),
                 state = Optional.presentIfNotNull(state))
 
